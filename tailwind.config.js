6175b663a735588da96f6c527b31aea94d3f7d7c
QuentinWidlocher <qwidlocher@gmail.com>
2022-07-07 18:51:04 +0200

    🐛  Fix daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,24 +9,24 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
-  daisyui: {
-    themes: [
-      {
-        light: {
-          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
-          primary: "var(--tg-theme-button-color)",
-          'primary-content': "var(--tg-theme-button-text-color)",
-          neutral: "var(--tg-theme-bg-color)",
-          'neutral-content': "var(--tg-theme-text-color)",
-        },
-        dark: {
-          ...require("daisyui/src/colors/themes")["[data-theme=dark]"],
-          primary: "var(--tg-theme-button-color)",
-          'primary-content': "var(--tg-theme-button-text-color)",
-          neutral: "var(--tg-theme-bg-color)",
-          'neutral-content': "var(--tg-theme-text-color)",
-        }
-      }
-    ],
-  },
+  // daisyui: {
+  //   themes: [
+  //     {
+  //       light: {
+  //         ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+  //         primary: "var(--tg-theme-button-color)",
+  //         'primary-content': "var(--tg-theme-button-text-color)",
+  //         neutral: "var(--tg-theme-bg-color)",
+  //         'neutral-content': "var(--tg-theme-text-color)",
+  //       },
+  //       dark: {
+  //         ...require("daisyui/src/colors/themes")["[data-theme=dark]"],
+  //         primary: "var(--tg-theme-button-color)",
+  //         'primary-content': "var(--tg-theme-button-text-color)",
+  //         neutral: "var(--tg-theme-bg-color)",
+  //         'neutral-content': "var(--tg-theme-text-color)",
+  //       }
+  //     }
+  //   ],
+  // },
 };
